5d9f5bd4610e05a327df689a9b80abf39ebc19a4
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-06-06 23:34:53 +0530

    final

---
+++ spammer.py
@@ -0,0 +1,42 @@
+import pyautogui  
+import time
+
+time.sleep(3)  
+
+# __________From script______________
+
+# # f = open("spam.txt", 'r') 
+# f = open("OIP.jpg", 'r') 
+ 
+# for word in f:
+#     pyautogui.typewrite(word)     
+#     pyautogui.press("enter")     
+#     time.sleep(0.1)
+
+
+# __________________custom_______________
+
+while True:
+    pyautogui.typewrite(":3")
+    pyautogui.press("enter")
+    time.sleep(0.1)
+
+# _________________copy paste____________________
+    
+# while True:
+#     pyautogui.keyDown('ctrl')
+#     pyautogui.keyDown("v")
+#     pyautogui.keyUp('ctrl')
+#     pyautogui.keyUp('v')
+#     time.sleep(0.1)
+#     pyautogui.press("enter")
+#     time.sleep(0.1)
+
+
+# while True:
+#     pyautogui.keyDown('ctrl')
+#     pyautogui.keyDown("v")
+    
+#     time.sleep(0.5)
+#     pyautogui.press("enter")
+#     time.sleep(0.5)
